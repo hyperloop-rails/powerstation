583f68bb099f8824a255017c3a7f35c68ae9c6ee
ICSE2018 <icse2018acm@gmail.com>
2017-11-14 15:32:47 +0000

    two api

--- string-matching.sh
+++ string-matching.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+# any? => exists?
+
+grep -r '\.any?' *
+
+# where.first? => find_by
+
+grep -r '\.where(.*)\.first' *
+
+# => *.except(order)
+
+# each.update => update_all
+# .count => size
+# .map => .pluck
+# pluck.sum => sum
+# .pluck + pluck => SQL UNION
+# if exists? find else create end => find_or_create_by 
