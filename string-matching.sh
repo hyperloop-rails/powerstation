a4d0f2566367d79933b4ad36206055f0149caf56
ICSE2018 <icse2018acm@gmail.com>
2017-11-14 15:39:17 +0000

    one line update

--- string-matching.sh
+++ string-matching.sh
@@ -11,8 +11,22 @@ grep -r '\.where(.*)\.first' *
 # => *.except(order)
 
 # each.update => update_all
+
 # .count => size
+
+grep -r '\.count' *
+
 # .map => .pluck
+
+grep -r '\.map' *
+
 # pluck.sum => sum
+
+grep -r '\.pluck(.*)\.sum' *
 # .pluck + pluck => SQL UNION
+
+grep -r '\.pluck(.*).*+.*\.pluck(.*)' *
+
 # if exists? find else create end => find_or_create_by 
+
+pcrep 
